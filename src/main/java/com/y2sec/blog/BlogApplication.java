ee947e8fd18dfa83db61805abaa6803879540c76
y2sec <bae3835@gmail.com>
2021-01-11 17:51:38 +0900

    Initial commit

---
+++ src/main/java/com/y2sec/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.y2sec.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/y2sec/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2sec.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
